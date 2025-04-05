4e958a5b60fed8388a804a8bd0b81aa02b7210fa
Henry Hsu <43148403+henrykohl@users.noreply.github.com>
2025-04-05 09:33:26 -0500

    file updated

--- Xray/ml/model/arch.py
+++ Xray/ml/model/arch.py
@@ -8,26 +8,34 @@ class Net(nn.Module):
         Creating custom CNN architecture for Image classification
         """
         super(Net, self).__init__()
+        # super().__init__()  # 也可
+        """
+        in: (4維) batch_size x 3 x 224 x 224
+        """
 
         self.convolution_block1 = nn.Sequential(
             nn.Conv2d(
                 in_channels=3, out_channels=8, kernel_size=(3, 3), padding=0, bias=True
             ),
+            ## |224 + 2*padding - kernel_size|/stride + 1 = 222
+            # out: (batch_size) x 8 x 222 x 222
             nn.ReLU(),
-            nn.BatchNorm2d(8),
+            nn.BatchNorm2d(8), # 對應 out_channels=8
         )
 
-        self.pooling11 = nn.MaxPool2d(2, 2)
+        self.pooling11 = nn.MaxPool2d(2, 2) # out: (batch_size) x 8 x 111 x 111
 
         self.convolution_block2 = nn.Sequential(
             nn.Conv2d(
                 in_channels=8, out_channels=20, kernel_size=(3, 3), padding=0, bias=True
             ),
+            ## |111 + 2*padding - kernel_size|/stride + 1 = 109
+            # out: (batch_size) x 20 x 109 x 109
             nn.ReLU(),
-            nn.BatchNorm2d(20),
+            nn.BatchNorm2d(20), # 對應 out_channels=20
         )
 
-        self.pooling22 = nn.MaxPool2d(2, 2)
+        self.pooling22 = nn.MaxPool2d(2, 2) # out: (batch_size) x 20 x 54 x 54
 
         self.convolution_block3 = nn.Sequential(
             nn.Conv2d(
@@ -37,11 +45,13 @@ class Net(nn.Module):
                 padding=0,
                 bias=True,
             ),
+            ## |54 + 2*padding - kernel_size|/stride + 1 = 54
+            # out: (batch_size) x 10 x 54 x 54
             nn.ReLU(),
-            nn.BatchNorm2d(10),
+            nn.BatchNorm2d(10), # 對應 out_channels=10
         )
 
-        self.pooling33 = nn.MaxPool2d(2, 2)
+        self.pooling33 = nn.MaxPool2d(2, 2) # out: (batch_size) x 10 x 27 x 27
 
         self.convolution_block4 = nn.Sequential(
             nn.Conv2d(
@@ -51,8 +61,10 @@ class Net(nn.Module):
                 padding=0,
                 bias=True,
             ),
+            ## |27 + 2*padding - kernel_size|/stride + 1 = 25
+            # out: (batch_size) x 20 x 25 x 25
             nn.ReLU(),
-            nn.BatchNorm2d(20),
+            nn.BatchNorm2d(20), # 對應 out_channels=20
         )
 
         self.convolution_block5 = nn.Sequential(
@@ -63,8 +75,10 @@ class Net(nn.Module):
                 padding=0,
                 bias=True,
             ),
+            ## |25 + 2*padding - kernel_size|/stride + 1 = 25
+            # out: (batch_size) x 32 x 25 x 25
             nn.ReLU(),
-            nn.BatchNorm2d(32),
+            nn.BatchNorm2d(32), # 對應 out_channels=23
         )
 
         self.convolution_block6 = nn.Sequential(
@@ -75,8 +89,10 @@ class Net(nn.Module):
                 padding=0,
                 bias=True,
             ),
+            ## |25 + 2*padding - kernel_size|/stride + 1 = 23
+            # out: (batch_size) x 10 x 23 x 23
             nn.ReLU(),
-            nn.BatchNorm2d(10),
+            nn.BatchNorm2d(10), # 對應 out_channels=10
         )
 
         self.convolution_block7 = nn.Sequential(
@@ -87,8 +103,10 @@ class Net(nn.Module):
                 padding=0,
                 bias=True,
             ),
+            ## |23 + 2*padding - kernel_size|/stride + 1 = 23
+            # out: (batch_size) x 10 x 23 x 23
             nn.ReLU(),
-            nn.BatchNorm2d(10),
+            nn.BatchNorm2d(10), # 對應 out_channels=10
         )
 
         self.convolution_block8 = nn.Sequential(
@@ -99,8 +117,10 @@ class Net(nn.Module):
                 padding=0,
                 bias=True,
             ),
+            ## |23 + 2*padding - kernel_size|/stride + 1 = 21
+            # out: (batch_size) x 14 x 21 x 21
             nn.ReLU(),
-            nn.BatchNorm2d(14),
+            nn.BatchNorm2d(14), # 對應 out_channels=14
         )
 
         self.convolution_block9 = nn.Sequential(
@@ -111,16 +131,22 @@ class Net(nn.Module):
                 padding=0,
                 bias=True,
             ),
+            ## |21 + 2*padding - kernel_size|/stride + 1 = 19
+            # out: (batch_size) x 16 x 19 x 19
             nn.ReLU(),
-            nn.BatchNorm2d(16),
+            nn.BatchNorm2d(16), # 對應 out_channels=16
         )
 
         self.gap = nn.Sequential(nn.AvgPool2d(kernel_size=4))
+        ## |19/4| = 4
+        # out: (batch_size) x 16 x 4 x 4
 
         self.convolution_block_out = nn.Sequential(
             nn.Conv2d(
                 in_channels=16, out_channels=2, kernel_size=(4, 4), padding=0, bias=True
             ),
+            ## |4 + 2*padding - kernel_size|/stride + 1 = 1
+            # out: (batch_size) x 2 x 1 x 1
         )
 
     def forward(self, x) -> float:
