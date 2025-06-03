7a3bb5f253072b40a61853cde4993b61e2266a89
Henry Hsu <43148403+henrykohl@users.noreply.github.com>
2025-06-03 11:42:04 +0800

    file updated

--- Xray/ml/model/model_service.py
+++ Xray/ml/model/model_service.py
@@ -21,7 +21,7 @@ svc = bentoml.Service(name=BENTOML_SERVICE_NAME, runners=[runner])
 
 @svc.api(input=Image(allowed_mime_types=["image/jpeg"]), output=Text())
 async def predict(img): # img 應該是 PIL.JpegImagePlugin.JpegImageFile 類型
-    b = io.BytesIO()
+    b = io.BytesIO() # _io.BytesIO 類型
 
     img.save(b, "jpeg")
 
@@ -29,14 +29,17 @@ async def predict(img): # img 應該是 PIL.JpegImagePlugin.JpegImageFile 類型
 
     my_transforms = bento_model.custom_objects.get(TRAIN_TRANSFORMS_KEY) ## 從 bento model 提取 轉型模式物件 
 
-    image = PILImage.open(io.BytesIO(im_bytes)).convert("RGB")
+    image = PILImage.open(io.BytesIO(im_bytes)).convert("RGB") # image 是 PIL.Image.Image 類型
 
-    image = torch.from_numpy(np.array(my_transforms(image).unsqueeze(0)))
+    image = torch.from_numpy(np.array(my_transforms(image).unsqueeze(0))) # my_transforms(image) 是 torch.Tensor 類型
+    # torch.from_numpy(np.array()) 似乎多此一舉
+    # my_transforms(image).unsqueeze(0) 的大小是 [1,3,224,224]
+    # image 的大小也是 [1,3,224,224]
 
-    image = image.reshape(1, 3, 224, 224)
+    image = image.reshape(1, 3, 224, 224) # torch.tensor 類型
 
-    batch_ret = await runner.async_run(image)
+    batch_ret = await runner.async_run(image) # (應該也是) torch.tensor 類型，大小為  [1,2]
 
-    pred = PREDICTION_LABEL[max(torch.argmax(batch_ret, dim=1).detach().cpu().tolist())]
+    pred = PREDICTION_LABEL[max(torch.argmax(batch_ret, dim=1).detach().cpu().tolist())] # tolist 轉 tensor 為 list
 
-    return pred
+    return pred # 結果為 字串
