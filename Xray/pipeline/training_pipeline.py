abbe1ec4b1d2ba1191d66d82d6dbb68806232db0
Henry Hsu <43148403+henrykohl@users.noreply.github.com>
2025-06-14 11:32:25 +0800

    file updated

--- Xray/pipeline/training_pipeline.py
+++ Xray/pipeline/training_pipeline.py
@@ -123,16 +123,16 @@ class TrainPipeline:
     ## Lec 5
     def start_model_evaluation(
         self,
-        model_trainer_artifact: ModelTrainerArtifact,
-        data_transformation_artifact: DataTransformationArtifact,
+        model_trainer_artifact: ModelTrainerArtifact,             # 來自 Lec 4
+        data_transformation_artifact: DataTransformationArtifact, # 來自 Lec 3
     ) -> ModelEvaluationArtifact:
         logging.info("Entered the start_model_evaluation method of TrainPipeline class")
 
         try:
             model_evaluation = ModelEvaluation(
                 data_transformation_artifact=data_transformation_artifact, # 來自 Lec 3
-                model_trainer_artifact=model_trainer_artifact, # 來自 Lec 4
-                model_evaluation_config=self.model_evaluation_config, 
+                model_trainer_artifact=model_trainer_artifact,             # 來自 Lec 4
+                model_evaluation_config=self.model_evaluation_config,      # class TrainPipeline 本身自帶
             )
 
             model_evaluation_artifact = model_evaluation.initiate_model_evaluation() # 測試資料後的準確性結果
